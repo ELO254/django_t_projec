48a7d7f352f380d3401c87fb98911ef6c9419091
ELO254 <elvisodhiambo255@gmail.com>
2023-03-13 11:45:19 +0300

    initial commit

---
+++ djangoProjectELO254TUESDAY/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for djangoProjectELO254TUESDAY project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProjectELO254TUESDAY.settings')
+
+application = get_asgi_application()
